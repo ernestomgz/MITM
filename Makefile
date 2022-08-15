b991fd2901afe991e69082ba02a7419fb7de9049
ernestomgz <ernestomrtg@gmail.com>
2022-08-15 22:13:00 +0200

    working on libraries

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	gcc main.c -o a.out -I/System/Volumes/Data/sgoinfre/goinfre/Perso/ernmarti/homebrew/Cellar/libnet/1.2/include -I/System/Volumes/Data/sgoinfre/goinfre/Perso/ernmarti/homebrew/opt/libpcap/include -v && ./a.out 
+	gcc main.c -o a.out -I/System/Volumes/Data/sgoinfre/goinfre/Perso/ernmarti/homebrew/Cellar/libnet/1.2/include -lpcap -v  && ./a.out 
 debug: Docker/
-	gcc main.c -Wall -g -o a.out -I/System/Volumes/Data/sgoinfre/goinfre/Perso/ernmarti/homebrew/Cellar/libnet/1.2/include -I/System/Volumes/Data/sgoinfre/goinfre/Perso/ernmarti/homebrew/opt/libpcap/include -v  && ./a.out 
+	gcc main.c -o a.out -I/System/Volumes/Data/sgoinfre/goinfre/Perso/ernmarti/homebrew/Cellar/libnet/1.2/include -lpcap -v -Wall -g && ./a.out 
--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pcap.h>
-#include <libnet.h>
+#include "pcap.h"
+#include "libnet.h"
 
 
 int main(int argc, char* argv[]){
-    printf("hello world");
+    char error_buffer[PCAP_ERRBUF_SIZE]; /* Size defined in pcap.h */
 
+    char* device = "any";  //this define the interface, can be eth0, wlan0 or any (all interfaces),
+    pcap_t* devices = pcap_create(device,error_buffer);
+
+    if(pcap_activate(devices)!=0){
+        printf("error activating");
+    }
+
+    /* Find a device */
+//    device = pcap_lookupdev(error_buffer);
+//    if (device == NULL) {
+//        printf("Error finding device: %s\n", error_buffer);
+//        return 1;
+//    }
+
+    //printf("Network device found: %s\n", device);
     return 0;
 
 }
