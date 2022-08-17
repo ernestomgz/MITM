1de629a9657ff433a2b3addb920c3f8e4cc14cf5
Galán <antoniogalanG15@gmail.com>
2022-08-17 18:47:36 +0200

    Cabecera con datos del fichero
    Descripción del fichero, autores, fecha y versión.

--- src/inquisitor.c
+++ src/inquisitor.c
@@ -1,3 +1,13 @@
+/**
+ * @file    inquisitor.c
+ * @author  Ernesto Martínez Gómez (@ernestomgz),
+ *          Antonio J. Galán Herrera (@15Galan)
+ * @brief   Proof of Concept of a 'Man In The Middle' attack ('ARP Spoofing').
+ * @date    2022-08-17
+ * @version 0.1
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
