f240732cb2267fbef6e45fb21af61cd604e9e1db
Galán <antoniogalanG15@gmail.com>
2022-08-22 14:51:59 +0200

    Función de lectura argumentos implementada
    El programa recibe los argumentos y
    los interpreta en el formato correcto.

--- src/inquisitor.c
+++ src/inquisitor.c
@@ -42,8 +42,80 @@ struct pcap_t *handle;                 // Pcap   handle
 // ---------------------------------------------------------------------------------------------------------------------
 
 
+/**
+ * @brief Set the Global Data objects.
+ * 
+ * @param argc  Number of arguments.
+ * @param argv  Arguments.
+ *
+ * @return int  0 if success, -1 if error.
+ */
+int setGlobalData(int argc, char *argv[]) {
+
+    // TODO: verbose mode ('-v')
+
+    // Check arguments cuantity.
+    if (argc != 5) {
+        printf("Usage: %s <Source IP> <Source MAC> <Destination IP> <Destination MAC>\n", argv[0]);
+        exit(0);
+    }
+
+    /*
+    'sscanf' it's like an inverse 'printf' function: it parse a string and extract data from it.
+    */
+
+    // Source IP and MAC addresses.
+    src_ip.s_addr = inet_addr(argv[1]);
+    sscanf(argv[2], "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+        &src_mac.ether_addr_octet[0], &src_mac.ether_addr_octet[1], &src_mac.ether_addr_octet[2],
+        &src_mac.ether_addr_octet[3], &src_mac.ether_addr_octet[4], &src_mac.ether_addr_octet[5]);
+
+    // Destination IP and MAC addresses.
+    dst_ip.s_addr = inet_addr(argv[3]);
+    sscanf(argv[4], "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+        &dst_mac.ether_addr_octet[0], &dst_mac.ether_addr_octet[1], &dst_mac.ether_addr_octet[2],
+        &dst_mac.ether_addr_octet[3], &dst_mac.ether_addr_octet[4], &dst_mac.ether_addr_octet[5]);
+    
+    // Broadcast MAC address.
+    memset(&bcast_mac, 0xff, sizeof(bcast_mac));
+    
+    return 0;
+}
+
+
+/**
+ * @brief   Main function.
+ * 
+ * @param   argc Number of arguments.
+ * @param   argv Arguments.
+ * 
+ * @return  0 if success, -1 if error.
+ */
 int main(int argc, char *argv[]) {
-    // TODO: start to code
+    // Set Global Data
+    setGlobalData(argc, argv);
+
+    // Print Global Data (test)
+    printf("src IP  : %s\n", inet_ntoa(src_ip));
+    printf("src MAC : %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",
+        src_mac.ether_addr_octet[0], src_mac.ether_addr_octet[1], src_mac.ether_addr_octet[2],
+        src_mac.ether_addr_octet[3], src_mac.ether_addr_octet[4], src_mac.ether_addr_octet[5]);
+
+    printf("dst IP  : %s\n", inet_ntoa(dst_ip));
+    printf("dst MAC : %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",
+        dst_mac.ether_addr_octet[0], dst_mac.ether_addr_octet[1], dst_mac.ether_addr_octet[2],
+        dst_mac.ether_addr_octet[3], dst_mac.ether_addr_octet[4], dst_mac.ether_addr_octet[5]);
+
+    printf("atk IP  : %s\n", inet_ntoa(atk_ip));
+    printf("atk MAC : %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",
+        atk_mac.ether_addr_octet[0], atk_mac.ether_addr_octet[1], atk_mac.ether_addr_octet[2],
+        atk_mac.ether_addr_octet[3], atk_mac.ether_addr_octet[4], atk_mac.ether_addr_octet[5]);
+
+    printf("bdt MAC : %hhx:%hhx:%hhx:%hhx:%hhx:%hhx\n",
+        bcast_mac.ether_addr_octet[0], bcast_mac.ether_addr_octet[1], bcast_mac.ether_addr_octet[2],
+        bcast_mac.ether_addr_octet[3], bcast_mac.ether_addr_octet[4], bcast_mac.ether_addr_octet[5]);
+
+    printf("\n");
 
     return 0;
 }
